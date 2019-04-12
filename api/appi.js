ca8e002cde956bca4f1cfd7cb45c850db2abc841
stteffano13 <tefo.aguayo@gmail.com>
2019-04-12 10:27:50 -0500

    Esquematizado el proyecto

---
+++ api/appi.js
@@ -0,0 +1,40 @@
+'Use strict'
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+//Notificaciones
+
+
+
+
+//cargar Rutas
+/*var user_routes = require('./routes/user');
+var nuevaOferta_routes = require('./routes/nuevaOferta');
+var administrador_rutes = require('./routes/administrador');
+var email = require('./routes/enviarCorreo');*/
+//var paypal =require('./routes/paypal');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json()); //convertir a json als peticiones
+
+//configurar cabeceras http
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+ 
+    next();
+});
+
+// rutas base
+/*app.use('/api', user_routes);
+app.use('/api', nuevaOferta_routes);
+app.use('/api', administrador_rutes);
+app.use('/api',email);*/
+
+
+//app.use('/api',paypal);
+
+module.exports = app; // hace referencia a la variable de express
+
---
+++ api/index.js
@@ -0,0 +1,36 @@
+'use strict'
+
+var bcrypt = require('bcrypt-nodejs');
+var moment = require('moment');
+
+
+
+
+// coenxion base
+var mongoose = require('mongoose');
+var app = require('./appi');
+var port = process.env.PORT || 3977;
+
+
+
+mongoose.connect('mongodb://localhost:27017/plataforma', (err, res) => {
+    if (err) {
+        throw err;
+    } else {
+        console.log("base de datos esta corriendo correctamente");
+        app.listen(port, function () {
+            console.log("servidor del api rest de mucsica ecuchando por el puerto", port);
+        });
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
