5c5272ab0d3c254b5ffd56277834151502ecbf8f
stteffano13 <tefo.aguayo@gmail.com>
2019-05-14 01:16:32 -0500

    Empezando estudiante

--- plataforma/src/app/estudiante/estudiante.component.ts
+++ plataforma/src/app/estudiante/estudiante.component.ts
@@ -1,5 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { MateriaService } from '../services/materia.services';
+import { MatriculaService } from '../services/matricula.services';
+import { AdministradorService } from '../services/administrador.services';
+import { DocenteService } from '../services/docente.services';
+import { NotaService } from '../services/nota.services';
+import { Nota } from '../models/nota';
+import { NotaBasica } from '../models/notaBasica';
+import { Calculable } from '../models/calculable';
+import { isNumber } from 'util';
+
+
 @Component({
   selector: 'app-estudiante',
   templateUrl: './estudiante.component.html',
@@ -7,9 +18,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EstudianteComponent implements OnInit {
 
-  constructor() { }
+
+  public  periodoLectivoActual;
+  constructor(private _materiaService: MateriaService,
+    private _administradorService: AdministradorService,
+    private _matriculaServices: MatriculaService,
+    private _notaService: NotaService,) { }
 
   ngOnInit() {
-  }
 
+    this.getPeriodoActual();
+  }
+  getPeriodoActual() {
+
+
+    this._administradorService.getPeriodoActual().subscribe(response => {
+      console.log("este es el periodo que vino", response.periodo)
+      if (response.periodo != undefined) {
+        this.periodoLectivoActual = response.periodo[0].periodo;
+
+      }
+    }, (err) => { console.log("Existen Complicaciones Intente mas tarde", err) }
+    );
+
+  }
 }
